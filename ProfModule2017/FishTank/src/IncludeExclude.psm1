98e903ecaf301ed621c121793338065db1db4133
Staffan Gustafsson <info@powercode-consulting.se>
2017-04-30 20:33:22 +0200

    IncludeExcludeFilter case insenstive by default

--- ProfModule2017/FishTank/src/IncludeExclude.psm1
+++ ProfModule2017/FishTank/src/IncludeExclude.psm1
@@ -5,10 +5,18 @@ class IncludeExcludeFilter {
     hidden [WildcardPattern[]] $excludes
 
     IncludeExcludeFilter([string[]] $includes, [string[]] $excludes) {
+        $this.Init($includes, $excludes, [WildcardOptions]::IgnoreCase)
+    }
+
+    IncludeExcludeFilter([string[]] $includes, [string[]] $excludes, [WildcardOptions] $options) {
+        $this.Init($includes, $excludes, $options)
+    }
+
+    hidden [void] Init([string[]] $includes, [string[]] $excludes, [WildcardOptions] $options) {
         if ($includes) {
             $includePatterns = [WildcardPattern[]]::new($includes.Length)
             for ($i = 0; $i -lt $includes.Length; ++$i) {
-                $includePatterns[$i] = [WildcardPattern]::new($includes[$i])
+                $includePatterns[$i] = [WildcardPattern]::new($includes[$i], $options)
             }
             $this.includes = $includePatterns
         }
@@ -16,7 +24,7 @@ class IncludeExcludeFilter {
         if ($excludes) {
             $excludePatterns = [WildcardPattern[]]::new($excludes.Length)
             for ($i = 0; $i -lt $excludes.Length; ++$i) {
-                $excludePatterns[$i] = [WildcardPattern]::new($excludes[$i])
+                $excludePatterns[$i] = [WildcardPattern]::new($excludes[$i], $options)
             }
             $this.excludes = $excludePatterns
         }
