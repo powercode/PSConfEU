ead21ba590d96f7d4873f8c37d2d6b9272b6ee41
Staffan Gustafsson <info@powercode-consulting.se>
2017-05-01 18:43:53 +0200

    titles on charts

--- Perf2017/3.FileSystem.ps1
+++ Perf2017/3.FileSystem.ps1
@@ -2,6 +2,6 @@ using module .\release\Perf
 [CmdletBinding()]
 param()
 
-Measure-FileSystemIteration -ov res
+Measure-FileSystemIteration -ov res -Path c:\windows\system32
 
-$res | Out-Chart -Property Kind, TimeMs -ChartType Column -Title 'File system enumeration'
+$res | Out-Chart -Property Kind, TimeMs -ChartType Column -Title "File system enumeration: Path = $path"
--- Perf2017/4.PipelineOutput.ps1
+++ Perf2017/4.PipelineOutput.ps1
@@ -3,4 +3,4 @@ using module .\release\Perf\Perf.psd1
 param([int] $Count = 10000)
 
 Measure-ObjectOutput -ov res -Count $count
-$res | Sort-Object TimeMS -Descending | Out-Chart -Property Kind, Ticks -ChartType Column -ChartSettings @{LabelFormatString = 'N0'} -Title "Write to pipeline"
+$res | Sort-Object TimeMS -Descending | Out-Chart -Property Kind, Ticks -ChartType Column -ChartSettings @{LabelFormatString = 'N0'} -Title "Write to pipeline: Count = $Count"
